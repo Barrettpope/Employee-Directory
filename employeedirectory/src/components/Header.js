a298c5a5710a5bfba1fb51342063215b1719047e
Barrett Pope <barrettpope94@gmail.com>
2020-10-21 17:27:09 -0400

    Modified content in <p> tag

--- employeedirectory/src/components/Header.js
+++ employeedirectory/src/components/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Header() {
     return < >
-    <
-    div className = "jumbotron jumbotron-fluid" >
+        <
+        div className = "jumbotron jumbotron-fluid" >
         <
         div className = "container" >
         <
